a1d4cfa56e58f92291ff52982d44a6691c66ad06
ellckid <zelyonuy17@gmail.com>
2023-09-16 21:33:19 +0300

    добавил еще типов для thunk

--- src/store/posts/posts.actions.ts
+++ src/store/posts/posts.actions.ts
@@ -2,16 +2,28 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import PostService from "../../services/postService";
 import { PostInfo } from "../../types/types";
 
-export const getPost = createAsyncThunk<PostInfo[]>('posts/getAll', async () => {
+export const getPost = createAsyncThunk<PostInfo[]>('posts/getAll', async (): Promise<PostInfo[]> => {
     return await PostService.getPostList()
 })
-export const getPostByTitle = createAsyncThunk<PostInfo[] | undefined, string>('posts/getByTitle', async (title: string) => {
-    if (title.trim()) {
-        return await PostService.getPostByTitle(title)
-    } else if (title.length == 0) {
-        return await PostService.getPostList()
+export const getPostByTitle = createAsyncThunk<PostInfo[] | undefined, string>('posts/getByTitle', async (title: string): Promise<PostInfo[]> => {
+    try {
+        if (title.trim()) {
+            return await PostService.getPostByTitle(title)
+        } else if (title.length == 0) {
+            return await PostService.getPostList()
+        } else {
+            return [{ id: 0, userId: 0, title: '', body: '' }]
+        }
+    } catch (error) {
+        return [{ id: 0, userId: 0, title: '', body: '' }]
     }
+
+
 })
-export const getPostById = createAsyncThunk<PostInfo, number>('posts/getById', async (id: number) => {
-    return await PostService.getPostById(id)
+export const getPostById = createAsyncThunk<PostInfo, number>('posts/getById', async (id: number): Promise<PostInfo> => {
+    try {
+        return await PostService.getPostById(id)
+    } catch (error) {
+        return { id: 0, userId: 0, title: '', body: '' }
+    }
 })
